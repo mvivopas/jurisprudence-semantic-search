005ac4f8bb520d87aa6f0d1c433d6914c9e9a2f0
maria.vivo <maria.vivo@iomed.es>
2023-06-29 08:20:05 +0200

    adapt class to read from config instead of input params

--- src/models/tfidf_model.py
+++ src/models/tfidf_model.py
@@ -4,6 +4,7 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 from ..data_preprocessor import JurisdictionPreprocessor
+from .utils import CONFIG_PATH, read_config
 
 
 class TFIDFModel():
@@ -11,19 +12,17 @@ class TFIDFModel():
         self.processor = JurisdictionPreprocessor()
         self.model_path = model_path
 
-    def fit_and_save(self,
-                     data,
-                     max_ratio=0.9,
-                     min_ratio=0.1,
-                     max_dim=800,
-                     to_save=True,
-                     out_vecs=None):
-        # Create TFIDF matrix and model
-        self.vectorizer = TfidfVectorizer(max_df=max_ratio,
-                                          min_df=min_ratio,
-                                          max_features=max_dim)
+    def fit_and_save(self, data, to_save=True, out_vecs=None):
 
-        self.tfidf_vectors = self.vectorizer.fit_transform(self.data)
+        # Read model parameter configuration
+        params = read_config(CONFIG_PATH)
+
+        # Create TFIDF matrix and model
+        self.vectorizer = TfidfVectorizer(max_df=params["max_ratio"],
+                                          min_df=params["min_ratio"],
+                                          max_features=params["max_dim"])
+
+        self.tfidf_vectors = self.vectorizer.fit_transform(data)
 
         if to_save:
             # save vectorizer
