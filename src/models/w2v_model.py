e0c16fb294ce49224579389f222a98eefe71b42b
maria.vivo <maria.vivo@iomed.es>
2023-06-29 08:21:30 +0200

    add w2v model class

---
+++ src/models/w2v_model.py
@@ -0,0 +1,38 @@
+import os
+import pickle
+
+import numpy as np
+from gensim.models import Word2Vec
+
+from ..data_preprocessor import JurisdictionPreprocessor
+from .utils import CONFIG_PATH, read_config
+
+class Word2VecModel():
+    def __init__(self, model_path):
+        self.processor = JurisdictionPreprocessor()
+        self.model_path = model_path
+        self.paths = read_config(CONFIG_PATH)["general"]
+
+    def fit_and_save(self,
+                     data,
+                     to_save=True):        
+        # Read model parameter configuration
+        params = read_config(CONFIG_PATH)["word2vec"]
+
+        self.model = Word2Vec(data, 
+                              size=params["size"], 
+                              window=params["window"], 
+                              min_count=params["min_count"], 
+                              workers=params["workers"])
+        
+        if to_save:
+            # Save model
+            model_out = os.path.join(self.paths["model_path"], 
+                                     params["model_file_name"])
+            self.model.save(model_out)
+
+            # Store just the words + their trained embeddings.
+            vec_out = os.path.join(self.paths["embedding_path"], 
+                                     params["vectors_file_name"])
+            word_vectors = self.model.wv
+            word_vectors.save(vec_out)
