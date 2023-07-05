ab08b9b3babf47c24e1dcac16631feac20d6e8f4
Maria Vivó Pascual <60606258+mvivopas@users.noreply.github.com>
2023-07-05 08:14:14 +0200

    add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='JurisprudenceSimilaritySearcher',
+    version='1.0',
+    description='Jurisprudence Similarity Searcher',
+    author='Maria Vivó',
+    author_email='maria.vivo1203@gmail.com',
+    packages=['models', 'scripts', 'src'],
+    install_requires=[],
+)
