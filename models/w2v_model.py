ce878028be3aef4645b12d2297d0f7a03e86ed20
Maria Vivó Pascual <60606258+mvivopas@users.noreply.github.com>
2023-07-06 09:26:38 +0200

    improve get document embedding function and format data for db insertion

--- models/w2v_model.py
+++ models/w2v_model.py
@@ -21,7 +21,9 @@ class Word2VecModel():
 
     def fit_and_save(self, data, to_save=True, table_path=None):
 
-        self.model = Word2Vec(data,
+        data_list = [d.split() for d in data]
+
+        self.model = Word2Vec(data_list,
                               vector_size=self.params["size"],
                               window=self.params["window"],
                               min_count=self.params["min_count"],
@@ -31,6 +33,9 @@ class Word2VecModel():
             # Save model
             self.model.save(self.model_path)
 
+            # Generate document vectorial representations
+            doc_embeddings = [self.get_doc_vector(doc) for doc in data]
+
             # Store just the words + their trained embeddings.
             vec_out = os.path.join(self.paths["embedding_path"],
                                    self.params["vectors_file_name"])
@@ -38,22 +43,32 @@ class Word2VecModel():
             word_vectors.save(vec_out)
 
             if table_path:
+                # format adequately to insert into db
+                dense_vector_list = [[vec.tolist()] for vec in doc_embeddings]
                 # save vectors into pgvector data base
                 db_manager = JurisdictionDataBaseManager()
-                db_manager("pgvector", table_path, word_vectors)
+                db_manager("pgvector", table_path, dense_vector_list)
 
     def load(self):
         self.model = Word2Vec.load(self.model_path)
-        self.index2word_set = set(self.model.wv.index2word)
+        # self.index2word_set = set(self.model.wv.index2word)
 
-    def get_query_embedding(self, query_text):
-        query_embedding = np.zeros((self.model.vector_size, ), dtype='float32')
-        query_text_list = query_text.split()
+    def get_doc_vector(self, document):
+        # Initialize an empty vector
+        aggregate_vector = np.zeros(self.model.vector_size)
+        word_list = document.split()
         word_count = 0
-        for word in query_text_list:
-            if word in self.index2word_set:
+
+        # Iterate over each word in the document
+        for word in word_list:
+            if word in self.model.wv.key_to_index:
+                # If the word is in the model's vocabulary
+                # add its vector to the aggregate
+                aggregate_vector += self.model.wv[word]
                 word_count += 1
-                query_embedding = np.add(query_embedding, self.model[word])
+
+        # Average the aggregate vector by dividing by the word count
         if word_count > 0:
-            query_embedding = np.divide(query_embedding, word_count)
-        return query_embedding
+            aggregate_vector = np.divide(aggregate_vector, word_count)
+
+        return aggregate_vector
