0d4d54c1c8c8d750d10dbfe56a8faae3c56dc26a
Maria Vivó Pascual <60606258+mvivopas@users.noreply.github.com>
2023-07-07 08:40:29 +0200

    adapt to db_manager changes and simplify

--- scripts/generate_app.py
+++ scripts/generate_app.py
@@ -1,44 +1,19 @@
-import json
 import os
 
 import faiss
 import numpy as np
-import psycopg2
 import streamlit as st
+from data_processing.data_storage import JurisdictionDataBaseManager
 
 from ..models.tfidf_model import TFIDFModel
+from ..models.w2v_model import Word2VecModel
 
 CURDIR = os.path.dirname(__file__)
 MODEL_PATH = os.path.join(CURDIR, "data/models/vectorizer.pickle")
-VECTOR_DB_SECRETS = "database_secrets.json"
 TOP_K = 5
+METHOD = "tfidf"
 
-
-# Connect to the PostgreSQL database
-def connect_to_database():
-    # load scrapper arguments
-    with open(VECTOR_DB_SECRETS) as f:
-        db_args = json.load(f)
-
-    conn = psycopg2.connect(
-        host="localhost",
-        port=db_args["port"],
-        database=db_args["database_name"],
-        user=db_args["user"],
-        password=db_args["password"],
-    )
-    return conn
-
-
-# Load the embeddings from the database
-def load_embeddings_from_database(conn):
-    cur = conn.cursor()
-    cur.execute("SELECT id, vector FROM tfidf_vectors")
-    results = cur.fetchall()
-    ids = np.array([i[0] for i in results])
-    embeddings = np.array([i[1] for i in results])
-    cur.close()
-    return ids, embeddings
+DICT_CATEGORY_MODEL = {"TfIdf": TFIDFModel(), "WordVector": Word2VecModel()}
 
 
 # Normalize the embeddings
@@ -48,7 +23,8 @@ def normalize_embeddings(embeddings):
 
 # Build the FAISS index
 def build_faiss_index(embeddings, ids):
-    index = faiss.IndexFlatIP(embeddings.shape[1])  # Cosine similarity index
+    # Cosine similarity index
+    index = faiss.IndexFlatIP(embeddings.shape[1])
     index.add(embeddings)
     return index
 
@@ -62,20 +38,25 @@ def perform_similarity_search(index, model, query_text, k):
 
 
 # Streamlit app
-def streamlit_app(conn, index, model, k):
+def streamlit_app(db_manager, k):
     st.title("Similar Document Search")
     st.write("Enter a new document:")
     new_document = st.text_input("")
 
-    if st.button("Search"):
-        cur = conn.cursor()
+    category = st.selectbox("Select a category", list(DICT_CATEGORY_MODEL))
+
+    model = DICT_CATEGORY_MODEL.get(category)
+    model.load()
+
+    ids, embeddings = db_manager.load_embeddings_from_pgvector_table(category)
+    index = build_faiss_index(embeddings, ids)
+
+    if st.button("Search") and category:
         top_k_ids = perform_similarity_search(index, model, new_document,
                                               k).tolist()[0]
-        cur.execute(f"""SELECT id, vector
-                FROM tfidf_vectors
-                WHERE id = ANY(ARRAY{top_k_ids})""")
-        similar_documents = cur.fetchall()
-        cur.close()
+
+        id, similar_documents = db_manager.load_embeddings_from_pgvector_table(
+            category, top_k_ids)
 
         st.write("Similar documents:")
         for document in similar_documents:
@@ -83,18 +64,12 @@ def streamlit_app(conn, index, model, k):
 
 
 # Main function
-def main(top_k):
-    conn = connect_to_database()
-    ids, embeddings = load_embeddings_from_database(conn)
-    index = build_faiss_index(embeddings, ids)
+def main(top_k, method):
+    db_manager = JurisdictionDataBaseManager()
+    db_manager.generate_connection("pgvector")
 
-    # init model
-    model = TFIDFModel(MODEL_PATH)
-    # load the model
-    model.load()
-
-    streamlit_app(conn, index, model, top_k)
+    streamlit_app(db_manager, top_k)
 
 
 if __name__ == "__main__":
-    main(TOP_K)
+    main(TOP_K, METHOD)
