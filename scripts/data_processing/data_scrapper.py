2278dda43194f2efcbcd502d32555dd29c489948
Maria Vivó Pascual <60606258+mvivopas@users.noreply.github.com>
2023-07-26 09:36:04 +0200

    add type hints and docstrings to functions in scrapper class

--- scripts/data_processing/data_scrapper.py
+++ scripts/data_processing/data_scrapper.py
@@ -2,19 +2,22 @@
 
 import os
 import re
+from typing import Tuple
 
 import numpy as np
-from selenium import webdriver
+from selenium.webdriver import Edge as EdgeDriver
+from selenium.webdriver import EdgeOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.edge.service import Service as EdgeService
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 # num requests will always be 4 as it is the maximum number of pages
 # in one search
-NUM_REQUESTS = 19
+NUM_REQUESTS = 20
 ROOT_URL = "https://www.poderjudicial.es"
 month_to_num = {
     "enero": '01',
@@ -39,11 +42,27 @@ class JurisdictionScrapper():
         self.edge_service = EdgeService(
             executable_path=EdgeChromiumDriverManager().install())
 
-        self.option = webdriver.EdgeOptions()
+        self.option = EdgeOptions()
         self.option.add_argument("start-maximized")
 
-    def __call__(self, date, textual_query, num_searches,
-                 output_path_general_links, output_path_pdf_links):
+    def __call__(self, date: str, textual_query: str, num_searches: int,
+                 output_path_general_links: str,
+                 output_path_pdf_links: str) -> set[str]:
+        """
+        Scrape general and PDF links to jurisprudences based on the
+        given parameters.
+
+        Parameters:
+        date (str): The date for filtering jurisprudences.
+        textual_query (str): The query text to search for in jurisprudences.
+        num_searches (int): The number of pagination requests to make.
+        output_path_general_links (str): The file path to save the general
+        links.
+        output_path_pdf_links (str): The file path to save the PDF links.
+
+        Returns:
+        set[str]: A set of unique general links to jurisprudences.
+        """
 
         links_set = set()
         for i in range(num_searches):
@@ -76,21 +95,45 @@ class JurisdictionScrapper():
 
         return links_set
 
-    def init_driver(self):
-        driver = webdriver.Edge(service=self.edge_service, options=self.option)
+    def init_driver(self) -> EdgeDriver:
+        """
+        Initialize and return a WebDriver instance.
+
+        Returns:
+        WebDriver: A WebDriver instance for web scraping.
+        """
+        driver = EdgeDriver(service=self.edge_service, options=self.option)
         return driver
 
-    def get_general_link_href(self, link):
-        """Get HREF from embedded HTML link information"""
-        element = link.get_attribute('innerHTML')
-        start_link = element.find("a href", 0)
-        start_quote = element.find('"', start_link)
-        end_quote = element.find('"', start_quote + 1)
-        link = element[start_quote + 1:end_quote]
+    def get_general_link_href(self, element: WebElement):
+        """
+        Extracts the href value from the given WebElement.
+
+        Parameters:
+        element (WebElement): The WebElement containing the link.
+
+        Returns:
+        str: The href value of the link.
+        """
+        # retrieve all text in the element
+        html_element = element.get_attribute('innerHTML')
+        # search for the start and end position of the href element
+        start_link_str = 'a href="'
+        start_link = html_element.find(start_link_str) + len(start_link_str)
+        end_link = html_element.find('"', start_link)
+        link = html_element[start_link:end_link]
         return link
 
-    def get_link_to_pdf_juris(self, general_link):
+    def get_link_to_pdf_juris(self, general_link: str) -> str:
+        """
+        Extracts the link to the PDF jurisprudence from the given general link.
 
+        Parameters:
+        general_link (str): The general link to the jurisprudence.
+
+        Returns:
+        str: The link to the PDF jurisprudence.
+        """
         driver = self.init_driver()
         wait = WebDriverWait(driver, 30)
 
@@ -103,27 +146,59 @@ class JurisdictionScrapper():
 
         # get link to jurisprudence pdf
         pdf_box_element = driver.find_element(By.ID, "objtcontentpdf")
-        html_element = pdf_box_element.get_attribute('innerHTML')
-        start_link_str = 'a href="'
-        start_link = html_element.find(start_link_str) + len(start_link_str)
-        end_link = html_element.find('"', start_link)
-        link = html_element[start_link:end_link]
+        link = self.get_general_link_href(pdf_box_element)
 
         driver.quit()
         return link
 
-    def get_general_links_to_juris(self,
-                                   root,
-                                   name,
-                                   num_requests,
-                                   date,
-                                   fecha_state=False):
+    def get_date_and_format(self, driver: EdgeDriver) -> str:
         """
-        Create a driver and access to the searcher, make the query with
-        all required conditions (date, location, organ, etc) run que query
-        and extract the links from the results
-        """
+        Extracts the last jurisprudence date from the web page and formats it.
 
+        Parameters:
+        driver (WebDriver): The WebDriver instance.
+
+        Returns:
+        str: The formatted last jurisprudence date (DD/MM/YYYY).
+        """
+        # Identify last element's title
+        xpath_content = "//div[starts-with(@id, 'jurisprudenciaresults_content-')]"  # noqa: E501
+        content = driver.find_elements(By.XPATH, xpath_content)
+        juris_title = content[-1].find_element(By.CLASS_NAME, "title").text
+
+        # Extract date from element title in format: <DD de MONTH de YYYY>
+        # and transform to desired format
+        date_parts = re.search(r'\d{2}\sde\s\w+\sde\s\d{4}',
+                               juris_title).group().split(' de ')
+
+        month_num = month_to_num.get(date_parts[1])
+        last_date = f'{date_parts[0]}/{month_num}/{date_parts[2]}'
+
+        return last_date
+
+    def get_general_links_to_juris(self,
+                                   root: str,
+                                   juris_topic: str,
+                                   num_requests: int,
+                                   date: str,
+                                   fecha_state: bool = False) \
+            -> Tuple[str, list[str]]:
+        """
+        Retrieves links to jurisprudence pdf in web based on the given
+        parameters.
+
+        Parameters:
+        root (str): The root URL of the jurisprudence searcher.
+        juris_topic (str): The topic of jurisprudences to search for.
+        num_requests (int): The number of pagination requests to make.
+        date (str): The date for filtering jurisprudences.
+        fecha_state (bool, optional): Whether to set the date for filtering.
+                                      Defaults to False.
+
+        Returns:
+        Tuple[str, List[str]]: A tuple containing the last jurisprudence date
+                               and a list of links to jurisprudences.
+        """
         driver = self.init_driver()
 
         wait = WebDriverWait(driver, 30)
@@ -194,9 +269,9 @@ class JurisdictionScrapper():
                 By.ID, "frmBusquedajurisprudencia_FECHARESOLUCIONHASTA")
             fecha.send_keys(date)
 
-        # Set query and search
+        # Set topic and search
         search = driver.find_element(By.ID, "frmBusquedajurisprudencia_TEXT")
-        search.send_keys(name)
+        search.send_keys(juris_topic)
         search.send_keys(Keys.RETURN)
 
         last_date = None
@@ -208,7 +283,7 @@ class JurisdictionScrapper():
                 EC.presence_of_element_located(
                     (By.ID, "jurisprudenciaresults_searchresults")))
 
-            # identify links
+            # identify and retrieve links
             link_elements = text.find_elements(By.CLASS_NAME, 'title')
             links_juris.extend(
                 [self.get_general_link_href(link) for link in link_elements])
@@ -218,17 +293,10 @@ class JurisdictionScrapper():
                 EC.presence_of_element_located((By.NAME, "gotopage")))
 
             # if last page select date of last sentence
+            # get last jurisprudence date to repeat the search
+            # from that date
             if i == int(num_requests) - 1:
-                xpath_content = "//div[starts-with(@id, 'jurisprudenciaresults_content-')]"  # noqa: E501
-                content = driver.find_elements(By.XPATH, xpath_content)
-                juris_title = content[-1].find_element(By.CLASS_NAME,
-                                                       "title").text
-
-                date_parts = re.search(r'\d{2}\sde\s\w+\sde\s\d{4}',
-                                       juris_title).group().split(' de ')
-
-                month_num = month_to_num.get(date_parts[1])
-                last_date = f'{date_parts[0]}/{month_num}/{date_parts[2]}'
+                last_date = self.get_date_and_format(driver)
 
             main.clear()
             main.send_keys("{}".format(i + 2))
