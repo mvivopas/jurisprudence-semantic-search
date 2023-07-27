fd4403409de40a6e726a8f97f114a4cc5c5b7a8f
Maria Vivó Pascual <60606258+mvivopas@users.noreply.github.com>
2023-07-27 19:59:27 +0200

    migrate regexs as constants + simplify + improve robustness-> extract_information_from_doc

--- scripts/data_processing/data_preprocessor.py
+++ scripts/data_processing/data_preprocessor.py
@@ -14,6 +14,19 @@ from pdfminer3.pdfpage import PDFPage
 
 nltk.download('stopwords')
 
+INFO_NOT_FOUND_STRING = 'Not provided'
+
+# REGEX PATTERNS
+CENDOJ_ID_PATTERN = re.compile(r'\d{20}')
+DATE_PATTERN = re.compile(r'Fecha:\s(\d{2}/\d{2}/\d{4})')
+PARTE_RECURRENTE_PATTERN = re.compile(r'(?i)(Parte recurrente/Solicitante:|'
+                                      r'Parte recurrente|Procurador)')
+FUNDAMENTOS_PATTERN = re.compile(
+    r'(F\s?U\s?N\s?D\s?A\s?M\s?E\s?N\s?T\s?O\s?S|RAZONAMIENTOS JURÍDICOS)')
+FALLO_PATTERN = re.compile(r'(?i)(F\W?A\W?L\W?L|PARTE DISPOSITIVA|'
+                           r'P A R T E D I S P O S I T I V A|firmamos)')
+COSTAS_PATTERN = re.compile(r'\bno\W+(?:\w+\W+){1,6}?costas\b')
+
 
 class JurisdictionPreprocessor():
     def __init__(self):
@@ -39,8 +52,16 @@ class JurisdictionPreprocessor():
 
         return dict_information
 
-    def extract_text_from_link(self, url):
-        """Extract text from PDF url"""
+    def extract_text_from_link(self, url: str) -> str:
+        """
+        Extracts text from a PDF URL.
+
+        Parameters:
+            url (str): The URL of the PDF to extract text from.
+
+        Returns:
+            str: The extracted text from the PDF.
+        """
         resource_manager = PDFResourceManager()
 
         fake_file_handle = io.StringIO()
@@ -71,107 +92,95 @@ class JurisdictionPreprocessor():
     def extract_information_from_doc(self, doc):
         """Extract information from document that will
         be stored in the database"""
-
         dict_info = dict()
 
-        # CENDOJ id of the document
-        pattern_id = re.search(r'(\d{20})', doc)
-        id_cendoj = pattern_id.group()
-        dict_info["id_cendoj"] = id_cendoj
+        # CENDOJ id
+        cendoj_id_match = CENDOJ_ID_PATTERN.search(doc).group()
+        dict_info["id_cendoj"] = cendoj_id_match
 
         # Date
-        start_año = doc.find('Fecha', 0) + 13
-        end_año = doc.find('\n', start_año)
-        año = int(doc[start_año:end_año])
-        dict_info["year"] = año
+        date_match = DATE_PATTERN.search(doc).group(1)
+        dict_info["date"] = date_match
 
         # Cuestiones part of document
-        if doc[0:2000].find('Cuestiones', 0) != -1:
-            start_cuest = doc.find('Cuestiones', 0) + 12
+        cuestiones_str = 'Cuestiones'
+        if doc[:2000].find(cuestiones_str, 0) != -1:
+            start_cuest = doc.find(cuestiones_str, 0) + len(cuestiones_str)
             end_cuest = doc.find('\n', start_cuest)
-            cuestiones = doc[start_cuest:end_cuest]
+            cuestiones = re.sub(r'\W+', ' ',
+                                doc[start_cuest:end_cuest]).strip()
         else:
-            cuestiones = ""
+            cuestiones = INFO_NOT_FOUND_STRING
 
         dict_info["cuestiones"] = cuestiones
 
         # Materia part of document
-        start_materia = doc.find('Materia', 0) + 8
-        end_materia = doc.find('\n', start_materia)
-        materia = doc[start_materia:end_materia]
+        materia_str = 'Materia'
+        match_materia = doc.find(materia_str, 0)
+        if match_materia == -1:
+            materia = INFO_NOT_FOUND_STRING
+        else:
+            start_materia = match_materia + len(materia_str)
+            end_materia = doc.find('\n', start_materia)
+            materia = re.sub(r'\W+', ' ',
+                             doc[start_materia:end_materia]).strip()
+
         dict_info["materia"] = materia
 
         # Parte recurrente/apelante part of document
-        pattern_recurrente = [
-            re.compile(r'(Parte recurrente/Solicitante:|Parte recurrente)'),
-            re.compile(r'\nProcurador')
-        ]
-
-        if re.search(pattern_recurrente[0], doc):
-            doc_loc_recurrente = [
-                re.search(pattern_recurrente[0], doc).span()[1],
-                re.search(pattern_recurrente[1], doc).span()[0]
-            ]
-
-            parte_recurrente = doc[int(doc_loc_recurrente[0]
-                                       ):int(doc_loc_recurrente[1])]
+        parte_recurrente_match = PARTE_RECURRENTE_PATTERN.search(doc)
+        if parte_recurrente_match:
+            start_recurrente = parte_recurrente_match.span(1)[-1]
+            end_recurrente = doc.find('\n', start_recurrente)
+            recurrente = doc[start_recurrente:end_recurrente]
+            parte_recurrente_match = re.sub(r'\W+', ' ', recurrente).strip()
         else:
-            parte_recurrente = "not_found"
+            parte_recurrente_match = INFO_NOT_FOUND_STRING
 
-        dict_info["parte_recurrente"] = parte_recurrente
+        dict_info["parte_recurrente"] = parte_recurrente_match
 
         # Parte recurrida/apelada part of document
-        start_recurrida = doc.find('recurrida', 0) + 10
-        end_recurrida = doc.find('\n', start_recurrida)
-        parte_recurrida = doc[start_recurrida:end_recurrida]
+        recurrida_str = 'Parte recurrida'
+        match_recurrida = doc.find(recurrida_str, 0)
+        if match_recurrida == -1:
+            parte_recurrida = INFO_NOT_FOUND_STRING
+        else:
+            start_recurrida = match_recurrida + len(recurrida_str)
+            end_recurrida = doc.find('\n', start_recurrida)
+            parte_recurrida = doc[start_recurrida:end_recurrida]
+
         dict_info["parte_recurrida"] = parte_recurrida
 
         # Antecedentes de hecho part of document
-        pattern_antecedentes = [
-            re.compile(r'ANTECEDENTES DE HECHO'),
-            re.compile(r'FUNDAMENTOS|F U N D A M E N T O S|'
-                       r'RAZONAMIENTOS JURÍDICOS')
-        ]
-
-        doc_loc_antecedentes = [
-            re.search(pattern_antecedentes[0], doc).span()[1],
-            re.search(pattern_antecedentes[1], doc).span()[0]
-        ]
-
-        antecedentes = doc[int(doc_loc_antecedentes[0]
-                               ):int(doc_loc_antecedentes[1])]
+        antecedentes_str = 'ANTECEDENTES DE HECHO'
+        match_antec = doc.find(antecedentes_str, 0)
+        match_fundamentos = FUNDAMENTOS_PATTERN.search(doc)
+        if match_antec == -1:
+            antecedentes = INFO_NOT_FOUND_STRING
+        else:
+            start_antecedentes = match_antec + len(antecedentes_str)
+            end_antecedentes = match_fundamentos.span()[0]
+            antecedentes = doc[start_antecedentes:end_antecedentes]
 
         dict_info["antecedentes"] = antecedentes
 
-        # FUNDAMENTOS DE DERECHO
-        pattern_fallo = [
-            re.compile(r'F\W?A\W?L\W?L|PARTE DISPOSITIVA|'
-                       r'P A R T E D I S P O S I T I V A'),
-            re.compile(r'ﬁrmamos')
-        ]
-
-        fund_fallo = doc[doc_loc_antecedentes[1]:len(doc)]
-        doc_loc_fundamentos = [
-            re.search(pattern_antecedentes[1], fund_fallo).span()[1],
-            re.search(pattern_fallo[0], fund_fallo).span()[0]
-        ]
-
-        fundamentos = fund_fallo[int(doc_loc_fundamentos[0]
-                                     ):int(doc_loc_fundamentos[1])]
-
+        # Fundamentos part
+        match_fallo = FALLO_PATTERN.search(doc)
+        fundamentos = doc[match_fundamentos.span()[1]:match_fallo.span()[0]]
         dict_info["fundamentos"] = fundamentos
 
-        # fallo previo
+        # Fallo previo
         first_fallo = self.sentido_fallo(antecedentes)
         dict_info["first_fallo"] = first_fallo
 
-        # fallo definitivo
-        target_fallo = self.sentido_fallo(fund_fallo)
+        doc_fundamentos_fallo = doc[end_antecedentes:]
+        # Fallo definitivo
+        target_fallo = self.sentido_fallo(doc_fundamentos_fallo)
         dict_info["target_fallo"] = target_fallo
 
-        # costas procesales
+        # Costas Procesales
         costas_pro = 1
-        if re.search(r'\bno\W+(?:\w+\W+){1,6}?costas\b', fund_fallo):
+        if COSTAS_PATTERN.search(doc_fundamentos_fallo):
             costas_pro = 0
 
         dict_info["costas_pro"] = costas_pro
